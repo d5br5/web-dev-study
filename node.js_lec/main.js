882aeb467f2b53e44fc5cd9b897850e0fd71e956
DohyungKim <ehgud456456@naver.com>
2020-11-23 20:35:05 +0900

    node auth done

--- node.js_lec/main.js
+++ node.js_lec/main.js
@@ -71,6 +71,10 @@ var app = http.createServer(function(request,response){
         });
       }
     } else if(pathname === '/create'){
+      if(authIsOwner(request,response)===false){
+        response.end('Login required!!');
+        return false;
+      }
       fs.readdir('./data', function(error, filelist){
         var title = 'WEB - create';
         var list = template.list(filelist);
@@ -89,6 +93,10 @@ var app = http.createServer(function(request,response){
         response.end(html);
       });
     } else if(pathname === '/create_process'){
+      if(authIsOwner(request,response)===false){
+        response.end('Login required!!');
+        return false;
+      }
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -103,6 +111,10 @@ var app = http.createServer(function(request,response){
           })
       });
     } else if(pathname === '/update'){
+      if(authIsOwner(request,response)===false){
+        response.end('Login required!!');
+        return false;
+      }
       fs.readdir('./data', function(error, filelist){
         var filteredId = path.parse(queryData.id).base;
         fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
@@ -129,6 +141,10 @@ var app = http.createServer(function(request,response){
         });
       });
     } else if(pathname === '/update_process'){
+      if(authIsOwner(request,response)===false){
+        response.end('Login required!!');
+        return false;
+      }
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -146,6 +162,10 @@ var app = http.createServer(function(request,response){
           });
       });
     } else if(pathname === '/delete_process'){
+      if(authIsOwner(request,response)===false){
+        response.end('Login required!!');
+        return false;
+      }
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -175,6 +195,7 @@ var app = http.createServer(function(request,response){
         response.end(html);
       });
     } else if(pathname==='/login_process'){
+
       var body = '';
       request.on('data', function(data){
           body = body + data;
@@ -196,6 +217,10 @@ var app = http.createServer(function(request,response){
           }
       });
     } else if (pathname === '/logout_process') {
+      if(authIsOwner(request,response)===false){
+        response.end('Login required!!');
+        return false;
+      }
       var body = '';
       request.on('data', function (data) {
         body = body + data;
