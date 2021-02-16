42f12e98d3eebbcdf8fa55e4bef4876109b14d02
DohyungKim <ehgud456456@naver.com>
2021-02-16 21:55:13 +0900

    datatype

---
+++ java_lec/DataOperation/Datatype.java
@@ -0,0 +1,8 @@
+public class Datatype{
+	public static void main(String[] args) {
+		System.out.println('d');
+		System.out.println("six");
+		
+		System.out.println("1111".length());
+	}
+}
---
+++ java_lec/DataOperation/StringApp.java
@@ -0,0 +1,11 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hs\ndf");
+		System.out.println("Hello, [[[name]]] ... bye".replace("[[[name]]]", "ds"));
+
+	}
+
+}
