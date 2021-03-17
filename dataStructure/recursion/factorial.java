8ceff11dbeed80fa49cff9474b594010031a19ea
DohyungKim <ehgud456456@naver.com>
2021-03-18 00:35:31 +0900

    recursion 5-1

--- dataStructure/recursion/factorial.java
+++ dataStructure/recursion/factorial.java
@@ -1,9 +1,22 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class factorial {
 
+	static int factorial(int n) {
+		if(n>0) return n*factorial(n-1);
+		else return 1;
+	}
+	
 	public static void main(String[] args) {
-		System.out.println("dfd");
+		Scanner sin = new Scanner(System.in);
+		
+		System.out.println("int insert");
+		
+		int x = sin.nextInt();
+		
+		System.out.println(x+" 팩토리얼은 "+factorial(x)+ "입니다.");
 
 	}
 
